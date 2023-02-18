05ccb8ff0485af5555259267c418d531096c958a
Collin Schneider <collin@tagsafe.io>
2023-02-18 12:38:21 -0800

    removes need for DB /config file

--- api/sqs-data-consumer/db.js
+++ api/sqs-data-consumer/db.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
-const config = require(__dirname + '/database/config/config.js');
 
 // your config file will be in your directory
 const Connect = async () => {
-  console.log('Sequelize Config: ', JSON.stringify(config));
-  const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
+  const { dbHost, dbDatabase, dbUsername, dbPassword, dbDialect  } = process.env;
+  const sequelize = new Sequelize(dbDatabase, dbUsername, dbPassword, {
+    host: dbHost,
     port: 5432,
     logging: msg => console.log(`DB LOG: ${msg}`),
-    maxConcurrentQueries: 100,
-    dialect: config.dialect,
+    // maxConcurrentQueries: 100,
+    dialect: dbDialect,
     // dialectOptions: { ssl: 'Amazon RDS' },
     pool: { maxConnections: 5, maxIdleTime: 30 },
     language: 'en'
