e69a857bf89ae6765e4e0314bf145a74ceca8e27
Zach Zimbler <zach@tagsafe.io>
2023-02-17 22:32:08 -0800

    It's working by not done...

--- api/sqs-data-consumer/app.js
+++ api/sqs-data-consumer/app.js
@@ -1,11 +1,12 @@
-const { Sequelize } = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/database/config/config.js')[env];
+{ PerformanceDataPayloadFormatter } = require("./performanceDataPayloadFormatter");
+//const { Sequelize } = require('sequelize');
+//const env = process.env.NODE_ENV || 'development';
+//const config = require(__dirname + '/database/config/config.js')[env];
 
-console.log(config);
+//console.log(config);
 
 // your config file will be in your directory
-sequelize = new Sequelize(config.database, config.username, config.password, {
+/*sequelize = new Sequelize(config.database, config.username, config.password, {
    host: config.host,
    port: 5432,
    logging: console.log,
@@ -32,4 +33,25 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error);
   sequelize.close()
-}
+}*/
+
+module.exports.consumeMessages = async (event, _context) => {
+  try {
+
+
+
+
+    console.log(`Received event:`);
+    console.log(JSON.stringify(event));
+    if (!event?.Records?.length) return 'No records';
+    console.log(`Processing ${event.Records.length} records.`);
+    for (const record of event.Records) {
+      const data = new PerformanceDataPayloadFormatter(record.body);
+      console.log(record.body);
+    }
+    return 'Successfully processed Records'
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
