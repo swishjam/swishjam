035738ae84d887a1ef549319d4b4decc3fac0d85
Collin Schneider <collin@tagsafe.io>
2023-02-27 11:14:33 -0800

    readme updates

--- database/config/config.js
+++ database/config/config.js
@@ -1,26 +1,20 @@
 require('dotenv').config()
-const { dbHost, dbDatabase, dbUsername, dbPassword, dbDialect } = process.env; 
+
+if(!process.env.DB_USERNAME) throw new Error('DB_USERNAME ENV variable is not defined.');
+if(!process.env.DB_PASSWORD) throw new Error('DB_PASSWORD ENV variable is not defined.');
+if(!process.env.DB_NAME) throw new Error('DB_NAME ENV variable is not defined.');
+if(!process.env.DB_HOST) throw new Error('DB_HOST ENV variable is not defined.');
+
+const config = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect:  'postgres',
+}
 
 module.exports = {
-  "test": {
-    "username": dbUsername,
-    "password": dbPassword,
-    "database": dbDatabase,
-    "host": dbHost,
-    "dialect":  dbDialect,
-  },
-  "development": {
-    "username": dbUsername,
-    "password": dbPassword,
-    "database": dbDatabase,
-    "host": dbHost,
-    "dialect":  dbDialect,
-  },
-  "production": {
-    "username": dbUsername,
-    "password": dbPassword,
-    "database": dbDatabase,
-    "host": dbHost,
-    "dialect":  dbDialect,
-  }
+  test: config,
+  development: config,
+  production: config
 }
