951cd8ff9daf0d121c745922ea8dfa306201129d
Zach Zimbler <zach@tagsafe.io>
2023-02-28 23:07:23 -0800

    login and registration

--- dashboard/src/components/Auth/SignUp.js
+++ dashboard/src/components/Auth/SignUp.js
@@ -40,15 +40,15 @@ const SignUp = () => {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
         <div className="bg-white py-8 px-4 sm:border sm:rounded-lg sm:px-10 shadow-sm">
-          <h2 class="text-2xl mb-6 text-gray-900">Sign in to your account</h2> 
+          <h2 class="text-2xl mb-6 text-gray-900">Register</h2> 
 
           <Formik
             initialValues={{
               email: '',
               password: '',
             }}
-            validationSchema={SignInSchema}
-            onSubmit={signIn}
+            validationSchema={SignUpSchema}
+            onSubmit={signUp}
           >
             {({ errors, touched }) => (
               <Form className="space-y-6">
@@ -89,22 +89,11 @@ const SignUp = () => {
                 </div>{/* end password input */}
 
                 <div>
-                  <div className="flex items-center justify-between">
-                    <div className="text-sm mb-4">
-                      <div
-                        onClick={() => setView(VIEWS.FORGOTTEN_PASSWORD)}
-                        className="font-medium text-swishjam hover:text-swishjam-dark cursor-pointer"
-                      >
-                        Forgot your password?
-                      </div>
-                    </div>
-                  </div>
-
                   <button
                     type="submit"
                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-swishjam hover:bg-swishjam-dark focus:outline-none focus:ring-2 focus:ring-offset-2"
                   >
-                    Sign in
+                    Register
                   </button>
                 </div>
 
@@ -112,78 +101,19 @@ const SignUp = () => {
             )}
           </Formik> 
           {errorMsg && <div className="text-red-600 text-sm text-center">{errorMsg}</div>}
+          {successMsg && <div className="text-black">{successMsg}</div>}
 
         </div>
       </div>
     
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
         <p class="text-sm text-gray-600">
-          Don't have an account? <span class="font-medium text-swishjam hover:text-swishjam-dark"><span className="cursor-pointer" onClick={() => setView(VIEWS.SIGN_UP)}>Sign up for free account</span></span>
+          Already have an account? <span class="font-medium text-swishjam hover:text-swishjam-dark"><span className="cursor-pointer" onClick={() => setView(VIEWS.SIGN_IN)}>Sign in</span></span>
         </p> 
       </div> 
     
     </div>
   )
-
-
-
-
-
-
-
-  return (
-    <div className="card">
-      <h2 className="w-full text-center">Create Account</h2>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={SignUpSchema}
-        onSubmit={signUp}
-      >
-        {({ errors, touched }) => (
-          <Form className="column w-full">
-            <label htmlFor="email">Email</label>
-            <Field
-              className={cn('input', errors.email && 'bg-red-50')}
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            {errors.email && touched.email ? (
-              <div className="text-red-600">{errors.email}</div>
-            ) : null}
-
-            <label htmlFor="email">Password</label>
-            <Field
-              className={cn('input', errors.password && touched.password && 'bg-red-50')}
-              id="password"
-              name="password"
-              type="password"
-            />
-            {errors.password && touched.password ? (
-              <div className="text-red-600">{errors.password}</div>
-            ) : null}
-
-            <button className="button-inverse w-full" type="submit">
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      {errorMsg && <div className="text-red-600">{errorMsg}</div>}
-      {successMsg && <div className="text-black">{successMsg}</div>}
-      <button
-        className="link w-full"
-        type="button"
-        onClick={() => setView(VIEWS.SIGN_IN)}
-      >
-        Already have an account? Sign In.
-      </button>
-    </div>
-  );
 };
 
 export default SignUp;
