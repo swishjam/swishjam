f55b5f496c5c773f3ffdefbe5208d8f600719e0c
Collin Schneider <collin@tagsafe.io>
2023-03-01 08:58:55 -0800

    put Dashboard in AuthenticatedView

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -1,30 +1,12 @@
 'use client';
-import Link from 'next/link';
 import Header from '@components/Header';
-import Auth from '@components/Auth';
-import LoadingFullScreen from '@components/LoadingFullScreen';
-import { useAuth, VIEWS } from '@components/AuthProvider';
 import DashboardView from '@/components/DashboardView';
+import AuthenticatedView from '@/components/AuthenticatedView';
 
 export default function Home() {
-  const { initial, user, view, signOut } = useAuth();
-
-  if (initial) {
-    return <LoadingFullScreen />;
-  }
-
-  if (view === VIEWS.UPDATE_PASSWORD) {
-    return <Auth view={view} />;
-  }
-
-  if (user) {
-    return (
-      <>
-        <Header />
-        <DashboardView />
-      </>
-    );
-  }
-
-  return <Auth view={view} />;
+  return (
+    <AuthenticatedView>
+      <DashboardView />
+    </AuthenticatedView>
+  );
 }
