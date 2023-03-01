f8b680cd04b182de5d2e9688f1b1c245e0e012d8
Zach Zimbler <zach@tagsafe.io>
2023-02-28 22:51:59 -0800

    Updated the login

--- dashboard/src/components/Auth/SignUp.js
+++ dashboard/src/components/Auth/SignUp.js
@@ -1,12 +1,11 @@
 'use client';
-
 import { useState } from 'react';
 import cn from 'classnames';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-
-import { useAuth, VIEWS } from 'src/components/AuthProvider';
-import supabase from 'src/lib/supabase-browser';
+import Logo from '@components/Logo';
+import { useAuth, VIEWS } from '@components/AuthProvider';
+import supabase from '@lib/supabase-browser';
 
 const SignUpSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -15,8 +14,8 @@ const SignUpSchema = Yup.object().shape({
 
 const SignUp = () => {
   const { setView } = useAuth();
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [successMsg, setSuccessMsg] = useState(null);
+  const [ errorMsg, setErrorMsg ] = useState(null);
+  const [ successMsg, setSuccessMsg ] = useState(null);
 
   async function signUp(formData) {
     const { error } = await supabase.auth.signUp({
@@ -30,6 +29,107 @@ const SignUp = () => {
       setSuccessMsg('Success! Please check your email for further instructions.');
     }
   }
+
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <div className="mx-auto w-52">
+          <Logo className="h-12 inline-block" words={true} />
+        </div>  
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <div className="bg-white py-8 px-4 sm:border sm:rounded-lg sm:px-10 shadow-sm">
+          <h2 class="text-2xl mb-6 text-gray-900">Sign in to your account</h2> 
+
+          <Formik
+            initialValues={{
+              email: '',
+              password: '',
+            }}
+            validationSchema={SignInSchema}
+            onSubmit={signIn}
+          >
+            {({ errors, touched }) => (
+              <Form className="space-y-6">
+                <div> {/* Email input */}
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <Field
+                      className={cn('input', errors.email && touched.email && 'border-red-400')}
+                      id="email"
+                      name="email"
+                      placeholder="Email address"
+                      type="email"
+                    />
+                    {errors.email && touched.email ? (
+                      <div className="text-red-600 mt-1 text-sm text-right">{errors.email}</div>
+                    ) : null}
+                  </div>
+                </div>{/* End Email input */}
+
+                <div>{/* password input */}
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <Field
+                      className={cn('input', errors.password && touched.password && 'border-red-400')}
+                      id="password"
+                      name="password"
+                      autoComplete="current-password"
+                      type="password"
+                    />
+                    {errors.password && touched.password ? (
+                      <div className="text-red-600 mt-1 text-sm text-right">{errors.password}</div>
+                    ) : null}
+                  </div>
+                </div>{/* end password input */}
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <div className="text-sm mb-4">
+                      <div
+                        onClick={() => setView(VIEWS.FORGOTTEN_PASSWORD)}
+                        className="font-medium text-swishjam hover:text-swishjam-dark cursor-pointer"
+                      >
+                        Forgot your password?
+                      </div>
+                    </div>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-swishjam hover:bg-swishjam-dark focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  >
+                    Sign in
+                  </button>
+                </div>
+
+              </Form>
+            )}
+          </Formik> 
+          {errorMsg && <div className="text-red-600 text-sm text-center">{errorMsg}</div>}
+
+        </div>
+      </div>
+    
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <p class="text-sm text-gray-600">
+          Don't have an account? <span class="font-medium text-swishjam hover:text-swishjam-dark"><span className="cursor-pointer" onClick={() => setView(VIEWS.SIGN_UP)}>Sign up for free account</span></span>
+        </p> 
+      </div> 
+    
+    </div>
+  )
+
+
+
+
+
+
 
   return (
     <div className="card">
