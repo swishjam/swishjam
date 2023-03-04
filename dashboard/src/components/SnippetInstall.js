8f6ddb2cd1ea05fdbbef59161db9f2b0529b66e2
Zach Zimbler <zach@tagsafe.io>
2023-03-03 17:36:16 -0800

    Install modal is visible

--- dashboard/src/components/SnippetInstall.js
+++ dashboard/src/components/SnippetInstall.js
@@ -11,9 +11,56 @@ export default function SnippetInstall() {
   return (
     <Card>
       <div className="flex">
-        <div className="h-full w-1/2" >
+        <div className="h-full w-1/2 pr-12" >
+          <h1 className="text-lg font-medium mb-2">Installation Instructions</h1>
           
-          1. Copy the code on the right into you website's header. 
+          <div className="flow-root">
+            <ul role="list" className="-mb-8">
+              <li>
+                <div className="relative pb-8">
+                  <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+                  <div className="relative flex space-x-3">
+                    <div>
+                      <span className="h-8 w-8 rounded-full bg-swishjam flex items-center justify-center ring-8 ring-white text-white">
+                        1
+                      </span>
+                    </div>
+                    <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                      <div>
+                        <p className="text-sm text-gray-500">Copy & embed the snippet on the right into the <span className="font-medium text-gray-900">very bottom of the `head`</span> on each page of your website or application.</p>
+                        <p className="text-xs text-gray-500 mt-1">*We keep our script tag really small and performant so it doesn't interfere with your site.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <div className="relative pb-8">
+                  <div className="relative flex space-x-3">
+                    <div>
+                      <span className="h-8 w-8 rounded-full bg-swishjam flex items-center justify-center ring-8 text-white ring-white">
+                        2
+                      </span>
+                    </div>
+                    <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                      <div>
+                        <p className="text-sm text-gray-500">That's it! Once deployed load your site and then come back to Swishjam to see your data. Your data will load below and this section will disappear</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+
+
+
+
+
+
+
         </div>
 
         <div className="h-full w-1/2 flex flex-col justify-center items-center" >
