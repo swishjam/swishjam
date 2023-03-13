215de5d7fbf57b3c2c2683930ed5b431a557cbeb
Zach Zimbler <zach@tagsafe.io>
2023-03-13 18:01:39 -0400

    Experience score chart display working pretty well. Need to clean up some things and get the page view count live vs static data

--- dashboard/src/components/ExperienceScoreCard.js
+++ dashboard/src/components/ExperienceScoreCard.js
@@ -5,9 +5,6 @@ import { Card, LineChart, AreaChart } from "@tremor/react";
 import LoadingSpinner from '@components/LoadingSpinner';
 import Link from 'next/link';
 
-const dataFormatter = (number) =>
-  `${Intl.NumberFormat("us").format(number).toString()}%`;
-
 const resScore = (score) => {
   return [
     {
@@ -23,40 +20,29 @@ const resScore = (score) => {
 
 const calculateTimeseries = (lcp, cls, fcp, fid) => {
   
-  console.log('calculateTimeseries', lcp, cls, fcp, fid)
-  
   if(lcp.timeseriesData.length > 1 && cls.timeseriesData.length > 1 && fcp.timeseriesData.length > 1 && fid.timeseriesData.length > 1) {
-    console.log('ata lcp', lcp)
-    console.log('ata fcp', fcp)
-    console.log('ata cls', cls)
-    console.log('ata fid', fid)
     const allTimeseriesArr = [
       ...lcp.timeseriesData,
       ...cls.timeseriesData,
       ...fcp.timeseriesData,
       ...fid.timeseriesData,
     ]; 
-    console.log('ataTimeseries', allTimeseriesArr)
     let timeSeriesLookup = {};
     allTimeseriesArr.map( ata => {
-      let count = 1;
-      if(timeSeriesLookup[ata.timestamp]?.count) {
-        count = timeSeriesLookup[ata.timestamp].count++ 
-      } 
+      let count = timeSeriesLookup[ata.timestamp] ? timeSeriesLookup[ata.timestamp].count + 1 : 1;
+        
       // calc total REX
       //timeSeriesLookup[ata.timestamp]
       let score = 0; 
-      console.log('ata', ata)
-      if(timeSeriesLookup.score) {
-         
-         score = timeSeriesLookup.score + ata.metric.weightedScore
+      if(timeSeriesLookup[ata.timestamp]?.score) {
+        score = timeSeriesLookup[ata.timestamp].score + ata.metric.weightedScore
       } else {
         score = ata.metric.weightedScore
       }
 
       if(timeSeriesLookup[ata.timestamp]) {
         timeSeriesLookup[ata.timestamp] = {
-          ...timeSerieslookup[ata.timestamp],
+          ...timeSeriesLookup[ata.timestamp],
           [ata.metric.key]:ata.metric,
           count,
           score
@@ -72,42 +58,15 @@ const calculateTimeseries = (lcp, cls, fcp, fid) => {
 
     let finalTimeseriesArr = [];
     for (const [key, value] of Object.entries(timeSeriesLookup)) {
-      console.log(`${key}: ${value}`);
+      value.date = key;
       finalTimeseriesArr.push(value)
     }
     
-    console.log(finalTimeseriesArr); 
-    // convert to Array and filter out counts less than 4 to remove shitty data
-
-
-/*const mostTdp  = Math.max(lcp.timeseriesData.length ,
-      cls.timeseriesData.length, fcp.timeseriesData.length, fid.timeseriesData.length);
-    const leastTdp  = Math.min(lcp.timeseriesData.length ,
-      cls.timeseriesData.length, fcp.timeseriesData.length, fid.timeseriesData.length);
-    const smallestTimeseries = arrMetrics.find(metr => metr.timeseriesData.length === leastTdp); 
-    //lcpFromatted = lcp.timeseriesData.map( ts => {return {...ts, lcp.}})
-    console.log('least tdp', leastTdp);
-    console.log('smallestTimeseries tdp', smallestTimeseries);
-    */
-    // find matching datew in the timseries data to the smallestTimeseries
-    //smallestTimeseries.timeseriesData.map( d => {
-    //})
-    
-    /*v.map( ata => {
-      lookup[ata.timestamp] = {...lookup[ata.timestamp], [ata.metric.key]:ata.metric}  
-    })*/ 
-    
-    // {
-    //  timestamp: ''.
-    //  expScore: lcp.weighted + cls.weighted + fcp.weighted + fid.weighted. 
-    //}
-    //}
-     
-
-
-    return 1
+    const filteredFinalTimeseries = finalTimeseriesArr.filter ( i => i.count >= 4 ) 
+    // console.log('filteredFinalTimeseries', filteredFinalTimeseries)
+    return filteredFinalTimeseries
   }
-  return null 
+  return [] 
 }
 
 const renderActiveShape = (props) => {
@@ -156,15 +115,7 @@ export default function ExperienceScoreCard({ lcp, cls, fcp, fid, pageViews }) {
     cls?.metric?.weightedScore+fcp?.metric?.weightedScore))
   const data = resScore(Math.ceil(experienceScore)); 
   const COLORS = ['#f1f5f9', (experienceScore >= 90 ? '#10b981': (experienceScore >= 50 ? '#eab308': '#f43f5e'))];
-  //const chartData =  
-  //console.log(experienceScore); 
-  //console.log(data); 
-  //console.log(lcp)
-  console.log(fcp)
-  console.log(cls)
-  console.log(lcp)
-  console.log(fid)
-  console.log(calculateTimeseries(lcp, cls, fcp, fid))
+  
   return (
     <Card >
         
@@ -198,45 +149,20 @@ export default function ExperienceScoreCard({ lcp, cls, fcp, fid, pageViews }) {
           </div> 
         </div>
         <div className="w-2/3 flex">
-          {/*<AreaChart
-            data={chartData}
-            dataKey="year"
-            categories={['Population growth rate']}
+          {calculateTimeseries(lcp, cls, fcp, fid).length > 0 ?
+          <AreaChart
+            data={calculateTimeseries(lcp, cls, fcp, fid)}
+            dataKey="date"
+            categories={['score']}
             colors={['blue']}
             showLegend={false}
             startEndOnly={true}
             marginTop="mt-12"
-            valueFormatter={dataFormatter}
+            valueFormatter={d => d.toLocaleString()}
             yAxisWidth="w-10"
-          />*/}
+          />:<CardLoading />}
         </div>
       </div>
-
-
-      {/*<CardLoading />*/}
     </Card>
   );
 }
-/*
-          <div>
-            <h3 className="text-medium pb-2">How It's Calculated</h3> 
-            <div class="relative pl-9">
-              <dt class="inline font-semibold text-gray-900">
-                <svg class="absolute top-1 left-1 h-5 w-5 text-swishjam" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M10 2.5c-1.31 0-2.526.386-3.546 1.051a.75.75 0 01-.82-1.256A8 8 0 0118 9a22.47 22.47 0 01-1.228 7.351.75.75 0 11-1.417-.49A20.97 20.97 0 0016.5 9 6.5 6.5 0 0010 2.5zM4.333 4.416a.75.75 0 01.218 1.038A6.466 6.466 0 003.5 9a7.966 7.966 0 01-1.293 4.362.75.75 0 01-1.257-.819A6.466 6.466 0 002 9c0-1.61.476-3.11 1.295-4.365a.75.75 0 011.038-.219zM10 6.12a3 3 0 00-3.001 3.041 11.455 11.455 0 01-2.697 7.24.75.75 0 01-1.148-.965A9.957 9.957 0 005.5 9c0-.028.002-.055.004-.082a4.5 4.5 0 018.996.084V9.15l-.005.297a.75.75 0 11-1.5-.034c.003-.11.004-.219.005-.328a3 3 0 00-3-2.965zm0 2.13a.75.75 0 01.75.75c0 3.51-1.187 6.745-3.181 9.323a.75.75 0 11-1.186-.918A13.687 13.687 0 009.25 9a.75.75 0 01.75-.75zm3.529 3.698a.75.75 0 01.584.885 18.883 18.883 0 01-2.257 5.84.75.75 0 11-1.29-.764 17.386 17.386 0 002.078-5.377.75.75 0 01.885-.584z" clip-rule="evenodd"></path>
-  </svg>
-                Largest Contentful Paint
-              </dt>
-              <dd class=""> Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.</dd>
-            </div>
-            <div class="relative pl-9">
-              <dt class="inline font-semibold text-gray-900">
-                <svg class="absolute top-1 left-1 h-5 w-5 text-swishjam" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M10 2.5c-1.31 0-2.526.386-3.546 1.051a.75.75 0 01-.82-1.256A8 8 0 0118 9a22.47 22.47 0 01-1.228 7.351.75.75 0 11-1.417-.49A20.97 20.97 0 0016.5 9 6.5 6.5 0 0010 2.5zM4.333 4.416a.75.75 0 01.218 1.038A6.466 6.466 0 003.5 9a7.966 7.966 0 01-1.293 4.362.75.75 0 01-1.257-.819A6.466 6.466 0 002 9c0-1.61.476-3.11 1.295-4.365a.75.75 0 011.038-.219zM10 6.12a3 3 0 00-3.001 3.041 11.455 11.455 0 01-2.697 7.24.75.75 0 01-1.148-.965A9.957 9.957 0 005.5 9c0-.028.002-.055.004-.082a4.5 4.5 0 018.996.084V9.15l-.005.297a.75.75 0 11-1.5-.034c.003-.11.004-.219.005-.328a3 3 0 00-3-2.965zm0 2.13a.75.75 0 01.75.75c0 3.51-1.187 6.745-3.181 9.323a.75.75 0 11-1.186-.918A13.687 13.687 0 009.25 9a.75.75 0 01.75-.75zm3.529 3.698a.75.75 0 01.584.885 18.883 18.883 0 01-2.257 5.84.75.75 0 11-1.29-.764 17.386 17.386 0 002.078-5.377.75.75 0 01.885-.584z" clip-rule="evenodd"></path>
-  </svg>
-                Largest Contentful Paint
-              </dt>
-              <dd class=""> Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.</dd>
-            </div>
-          
-          </div>*/
