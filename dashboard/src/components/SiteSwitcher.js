caa5bfcb96ec622612c8d986dae1a200c580a78b
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-04-08 17:54:16 -0700

    fix uncentered project dropdown

--- dashboard/src/components/SiteSwitcher.js
+++ dashboard/src/components/SiteSwitcher.js
@@ -22,7 +22,7 @@ export default function SiteSwitcher(props) {
     <div {...props}>
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          <Menu.Button className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
             {currentProject?.name || <div className="mt-1"><LoadingSpinner size={4} /></div>} 
             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400 mt-1" aria-hidden="true" />
           </Menu.Button>
