4f8d7b1bbf14df4e0fa2cac7e684ab2b0389c0c6
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-04-16 10:52:14 -0700

    fix WPT init log

--- dashboard/src/pages/api/run-speed-test.js
+++ dashboard/src/pages/api/run-speed-test.js
@@ -27,12 +27,13 @@ export default async (req, res) => {
     console.error(err);
     return res.status(500).json({ error: `Invalid URL provided: ${url}.` });
   }
-  const queryParams = new URLSearchParams({ 
-    url: derivedUrl, 
-    k: process.env.WEB_PAGE_TEST_API_KEY, 
-    label: derivedLabel, 
-    lighthouse: includeLighthouse ? 1 : 0, 
-    video: includeVideo ? 1 : 0, 
+
+  const params = {
+    url: derivedUrl,
+    k: process.env.WEB_PAGE_TEST_API_KEY,
+    label: derivedLabel,
+    lighthouse: includeLighthouse ? 1 : 0,
+    video: includeVideo ? 1 : 0,
     fvonly: includeRepeatView ? 0 : 1,
     mobile: mobile ? 1 : 0,
     timeline: includeDevToolsTimeline ? 1 : 0,
@@ -40,9 +41,10 @@ export default async (req, res) => {
     metadata: JSON.stringify({ projectKey, email }),
     // location: 'Dulles:Chrome.Cable',
     pingback: `https://${process.env.WEB_PAGE_TEST_WEBHOOK_HOST || 'app.swishjam.com'}/api/speed-tests/webhook`,
-    f: 'json' 
-  });
-  console.log('Enqueueing WPT speed test: ', JSON.stringify(queryParams));
+    f: 'json'
+  };
+  const queryParams = new URLSearchParams(params);
+  console.log('Enqueueing WPT speed test: ', JSON.stringify(params));
   const result = await fetch(`https://www.webpagetest.org/runtest.php?${queryParams}`, { method: 'POST' });
   if (result.status === 200) {
     const response = await result.json();
