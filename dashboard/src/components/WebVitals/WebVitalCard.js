7d10632d2b74b25d960555aea650ff741df77182
Zach Zimbler <zach@tagsafe.io>
2023-04-26 12:22:05 -0400

    Added view details and cleaned up the CWV card a bit

--- dashboard/src/components/WebVitals/WebVitalCard.js
+++ dashboard/src/components/WebVitals/WebVitalCard.js
@@ -5,6 +5,7 @@ import { formattedMsOrSeconds } from '@lib/utils';
 import { cwvMetricBounds } from '@lib/cwvCalculations';
 import MetricGauge from '@/components/WebVitals/MetricGauge';
 import Link from 'next/link';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
 const CardIcon = (iconType) => {
   return {
@@ -34,40 +35,54 @@ export default function WebVitalCard({ title, accronym, percentileValue, numReco
       <>
         <dt className="flex items-center gap-x-3 text-base leading-7 text-gray-900">
           {CardIcon(accronym)}
-          <span className='hover:underline'>
-            <Link href={`/cwv/${accronym}`}>{title}</Link>
+          <span className='hover:underline hover:text-swishjam duration-300 transition'>
+            <Link
+              class="hover:text-swishjam"
+              href={`/cwv/${accronym}`}
+            >{title}</Link>
           </span>
         </dt>
         {typeof numRecordsInPercentileCalculation === 'number' ?
                   numRecordsInPercentileCalculation > 0 ? (
-                    <>
+                    <div className='mt-2'>
                       <Metric color={percentileColor}>
                         {accronym === 'CLS' ? parseFloat(percentileValue).toFixed(4) : formattedMsOrSeconds(percentileValue)}
                       </Metric>
                       <div className='mt-4'>
                         <MetricGauge accronym={accronym} percentileValue={percentileValue} />
                       </div>
-                    </>
+                    </div>
                   ) : <Metric>--</Metric> :
                   <div className='animate-pulse w-20 h-10 bg-gray-200 rounded'></div>
         }
         {formattedBarChartData === undefined ? <div className='animate-pulse w-full h-48 mt-4 bg-gray-200 rounded'></div> : 
             formattedBarChartData.length > 0 ? (
-              <BarChart
-                data={formattedBarChartData}
-                dataKey="date"
-                categories={['Good', 'Needs Improvement', 'Poor']}
-                colors={['green', 'yellow', 'red']}
-                showLegend={false}
-                startEndOnly={true}
-                valueFormatter={value => `${value}%`}
-                height="h-48"
-                marginTop="mt-4"
-                stack={true}
-                maxValue={100.0}
-                showYAxis={false}
-                showAnimation={false}
-              />
+              <>
+                <BarChart
+                  data={formattedBarChartData}
+                  dataKey="date"
+                  categories={['Good', 'Needs Improvement', 'Poor']}
+                  colors={['green', 'yellow', 'red']}
+                  showLegend={false}
+                  startEndOnly={true}
+                  valueFormatter={value => `${value}%`}
+                  height="h-48"
+                  marginTop="mt-4"
+                  stack={true}
+                  maxValue={100.0}
+                  showYAxis={false}
+                  showAnimation={false}
+                />
+                <div class='flex justify-end mt-4'>
+                  <Link
+                    href={`/cwv/${accronym}`}
+                    className='hover:text-swishjam duration-300 transition px-2.5 py-1 text-xs font-semibold text-gray-900'
+                  >
+                    View Details
+                    <span className='text-sm pl-2'>&#8250;</span>
+                  </Link>
+                </div>
+              </>
             ) : (
               <div className='flex justify-center items-center py-12'>
                 <Text>No data available for timeframe</Text>
