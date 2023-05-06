3dd477f9bd7c17911b84e89d88e72fd6680c4b86
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-05-06 13:11:11 -0700

    completes filmstrip video work

--- dashboard/src/app/lab-tests/results/[id]/overview/page.js
+++ dashboard/src/app/lab-tests/results/[id]/overview/page.js
@@ -3,10 +3,12 @@
 import { useEffect, useState } from "react";
 import AuthenticatedView from "@/components/AuthenticatedView";
 import DetailsHeader from "@/components/LabTests/DetailsHeader";
-import { formattedMsOrSeconds } from "@/lib/utils";
 import { WebPageTestResults } from "@/lib/web-page-test-results-parser";
 import LighthouseScore from "@/components/WebPageTest/LighthouseScore";
 import Filmstrip from "@/components/WebPageTest/Filmstrip";
+import FilmstripVideo from "@/components/WebPageTest/FilmstripVideo";
+import { FilmIcon } from "@heroicons/react/24/outline";
+import { formattedMsOrSeconds } from "@/lib/utils";
 
 const GOOD_NEEDS_IMPROVEMENT_POOR_TIERS = {
   LargestContentfulPaint: {
@@ -38,6 +40,7 @@ const GOOD_NEEDS_IMPROVEMENT_POOR_TIERS = {
 export default function Overview({ params }) {
   const { id } = params;
   const [webPageTestResults, setWebPageTestResults] = useState();
+  const [filmstripIsExpanded, setFilmstripIsExpanded] = useState(false);
 
   useEffect(() => {
     fetch(`https://www.webpagetest.org/jsonResult.php?test=${id}&breakdown=${1}`)
@@ -58,18 +61,29 @@ export default function Overview({ params }) {
         <div className='text-center'>
           <span className='block text-lg font-medium text-gray-700'>Lighthouse Performance Score</span>
           {webPageTestResults
-            ? <LighthouseScore score={webPageTestResults.lighthouseScore()} size='medium' />
+            ? <LighthouseScore score={webPageTestResults.lighthouseScore()} size='large' />
             : <div className='h-24 w-24 animate-pulse bg-gray-200 rounded-full mt-2 m-auto' />
           }
         </div>
-        <Filmstrip 
-          filmstrip={webPageTestResults?.filmstrip()} 
-          performanceMetrics={{
-            LargestContentfulPaint: webPageTestResults?.performanceData().LargestContentfulPaint,
-            FirstContentfulPaint: webPageTestResults?.performanceData().firstContentfulPaint,
-            TimeToFirstByte: webPageTestResults?.performanceData().TimeToFirstByte,
-          }} 
-        />
+        <div className='w-[50vw] my-4 m-auto'>
+          <FilmstripVideo 
+            filmstrip={webPageTestResults?.filmstrip({ filledIn: false })} 
+            performanceMetrics={{
+              LargestContentfulPaint: webPageTestResults?.performanceData().LargestContentfulPaint,
+              FirstContentfulPaint: webPageTestResults?.performanceData().firstContentfulPaint,
+              TimeToFirstByte: webPageTestResults?.performanceData().TimeToFirstByte,
+            }} 
+          />
+        </div>
+        <div className={`w-full mb-4 ${filmstripIsExpanded ? '' : 'border-b border-gray-200'}`}>
+          <span
+            className='text-sm text-gray-700 cursor-pointer hover:underline mt-2 mb-4 px-10'
+            onClick={() => setFilmstripIsExpanded(!filmstripIsExpanded)}
+          >
+            {filmstripIsExpanded ? 'Hide' : 'Show'} Filmstrip <FilmIcon className='h-4 w-4 inline-block ml-1' />
+          </span>
+          {filmstripIsExpanded && <Filmstrip filmstrip={webPageTestResults?.filmstrip()} />}
+        </div>
         <div className='grid grid-cols-3 gap-4'>
           {[
             ['Largest Contentful Paint', 'LargestContentfulPaint'],
--- dashboard/src/components/WebPageTest/Filmstrip.js
+++ dashboard/src/components/WebPageTest/Filmstrip.js
@@ -1,48 +1,12 @@
-import { useEffect, useState, useRef } from "react";
-import Modal from "@/components/utils/Modal";
 import { usePopperTooltip } from "react-popper-tooltip";
 import { formattedMsOrSeconds } from "@/lib/utils"
-import { VideoCameraIcon } from "@heroicons/react/24/outline";
 
-const filledInFilmstrip = filmstrip => {
-  const filmstripObject = {};
-  filmstrip.forEach(({ time, image, VisuallyComplete }) => filmstripObject[parseFloat(time)] = { time, image, VisuallyComplete } );
-  
-  const sortedFilmstripTimestamps = Object.keys(filmstripObject).map(ts => parseFloat(ts)).sort((a, b) => parseFloat(a.time) - parseFloat(b.time));
-  const maxTs = sortedFilmstripTimestamps[sortedFilmstripTimestamps.length - 1];
-  for (let i = 0; i <= maxTs; i += 500) {
-    if (!filmstripObject[i]) {
-      const closestTs = sortedFilmstripTimestamps.find((ts, j) => i >= ts && i < sortedFilmstripTimestamps[j + 1]);
-      filmstripObject[i] = { ...filmstripObject[closestTs], time: i };
-    }
-  }
-  return Object.values(filmstripObject);
-}
-
-export default function Filmstrip({ filmstrip, performanceMetrics, includeVideoPlayer = true }) {
-  const [displayVideoModal, setDisplayVideoModal] = useState(false);
-  const formattedFilmstrip = filmstrip && filledInFilmstrip(filmstrip);
-
+export default function Filmstrip({ filmstrip }) {
   return (
     <>
-      {includeVideoPlayer && (
-        <span 
-          className='block text-sm text-gray-700 mb-2 px-4 cursor-pointer hover:underline flex items-center'
-          onClick={() => setDisplayVideoModal(true)}
-        >
-          Play video <VideoCameraIcon className='ml-1 h-3 w-3 text-gray-700 inline-block' />
-        </span>
-      )}
-      {displayVideoModal && (
-        <VideoPlayerModal 
-          filmstrip={formattedFilmstrip} 
-          performanceMetrics={performanceMetrics} 
-          onClose={() => setDisplayVideoModal(false)} 
-        />
-      )}
-      <div className='flex items-center w-full overflow-x-scroll mb-4 px-4'>
+      <div className='flex items-center w-full overflow-x-scroll my-4 px-4'>
         {filmstrip
-          ? formattedFilmstrip.map(({ image, time }, i) => <FilmstripItem image={image} time={time} key={i} />
+          ? filmstrip.map(({ image, time }, i) => <FilmstripItem image={image} time={time} key={i} />
           ) : (
             Array.from({ length: 10 }).map((_, i) => (
               <div className='text-center mr-2' key={i}>
@@ -66,7 +30,7 @@ const FilmstripItem = ({ image, time }) => {
         <img src={image} className='rounded-lg shadow-md border border-gray-100 min-w-[200px]' />
       </div>
       {visible && (
-        <div ref={setTooltipRef} {...getTooltipProps()}>
+        <div ref={setTooltipRef} {...getTooltipProps({ className: 'z-20' })}>
           <div className='bg-white p-4 border border-gray-100 shadow-md rounded-md flex items-center justify-center'>
             <div>
               <h4 className='block text-lg font-medium text-gray-700 mb-2 text-center'>{formattedMsOrSeconds(time)}</h4>
@@ -76,114 +40,6 @@ const FilmstripItem = ({ image, time }) => {
           <div {...getArrowProps({ className: 'tooltip-arrow' })} />
         </div>
       )}
-    </>
-  )
-}
-
-const VideoPlayerModal = ({ filmstrip, performanceMetrics, onClose }) => {
-  const [currentFilmstripFrameIndex, setCurrentFilmstripFrameIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [playbackSpeed, setPlaybackSpeed] = useState(1);
-
-  const currentFilmstripItem = filmstrip[currentFilmstripFrameIndex];
-  const nextFilmstripItem = filmstrip[currentFilmstripFrameIndex + 1];
-
-  if (isPlaying && nextFilmstripItem) {
-    const msUntilNextFrame = nextFilmstripItem.time - currentFilmstripItem.time;
-    setTimeout(() => {
-      setCurrentFilmstripFrameIndex(currentFilmstripFrameIndex + 1);
-    }, msUntilNextFrame / playbackSpeed)
-  } else if (isPlaying && !nextFilmstripItem) {
-    setIsPlaying(false);
-  }
-
-  return (
-    <>
-      <Modal 
-        isOpen={true}
-        onClose={onClose}
-        content={(
-          <div className='flex justify-center items-center m-4'>
-            <div className='text-center'>
-              <div className='relative'>
-                {!isPlaying && (
-                  <div
-                    className='absolute flex h-full w-full items-center justify-center bg-black text-white text-4xl opacity-25 rounded-lg cursor-pointer hover:opacity-50 hover:text-6xl transition-all duration-200'
-                    onClick={() => {
-                      setCurrentFilmstripFrameIndex(0)
-                      setIsPlaying(true)
-                    }}
-                  >
-                    ▶️
-                  </div>
-                )}
-                <img src={currentFilmstripItem.image} className='rounded-lg shadow-md border border-gray-100 min-w-[400px]' />
-              </div>
-              <VideoPlayerTimer 
-                isRunning={isPlaying} 
-                time={currentFilmstripItem.time} 
-                duration={filmstrip[filmstrip.length - 1].time} 
-                playbackSpeed={playbackSpeed}
-                performanceMetrics={performanceMetrics}
-              />
-              <div className='flex items-center justify-center mt-4 grid grid-cols-5 gap-x-4'>
-                {[0.25, 0.5, 0.75, 1, 2].map(speed => (
-                  <div 
-                    className={`cursor-pointer px-4 py-1 text-sm rounded-md text-gray-700 hover:bg-gray-200 hover:text-gray-900 ${playbackSpeed === speed ? 'bg-gray-200' : ''}`} 
-                    onClick={() => setPlaybackSpeed(speed)}
-                    key={speed}
-                  >
-                    {speed}x
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-      />
-    </>    
-  )
-}
-
-const VideoPlayerTimer = ({ time, playbackSpeed, performanceMetrics, isRunning, duration, increments = 25 }) => {
-  const percentComplete = time / duration * 100;
-  const timerRef = useRef(null);
-  const durationIndicatorRef = useRef(null);
-  const intervalRef = useRef(null);
-
-  useEffect(() => {
-    if (isRunning) {
-      intervalRef.current = setInterval(() => {
-        const currentTime = parseInt(timerRef.current.dataset.time);
-        timerRef.current.innerText = formattedMsOrSeconds(currentTime + increments);
-        timerRef.current.dataset.time = currentTime + increments;
-        durationIndicatorRef.current.style.width = `${(currentTime / duration) * 100}%`;
-      }, increments / playbackSpeed);
-    } else {
-      clearInterval(intervalRef.current);
-    }
-  }, [isRunning])
-
-  return (
-    <>
-      <div className='w-full h-6 m-1 rounded relative'>
-        <div className='absolute top-0 h-full w-1 bg-blue-500 z-10' style={{ left: `${(performanceMetrics.TimeToFirstByte / duration) * 100}%` }} />
-        <div className='absolute top-6 text-gray-700 text-sm' style={{ left: `${(performanceMetrics.TimeToFirstByte / duration) * 100}%` }}>
-          TTFB
-        </div>
-        <div className='absolute top-0 h-full w-1 bg-yellow-500 z-10' style={{ left: `${(performanceMetrics.FirstContentfulPaint / duration) * 100}%` }} />
-        <div className='absolute top-6 text-gray-700 text-sm' style={{ left: `${(performanceMetrics.FirstContentfulPaint / duration) * 100}%` }}>
-          FCP
-        </div>
-        <div className='absolute top-0 h-full w-1 bg-yellow-500 z-10' style={{ left: `${(performanceMetrics.LargestContentfulPaint / duration) * 100}%` }} />
-        <div className='absolute top-6 text-gray-700 text-sm' style={{ left: `${(performanceMetrics.LargestContentfulPaint / duration) * 100}%` }}>
-          LCP
-        </div>
-        <div className='bg-swishjam h-full rounded' ref={durationIndicatorRef} style={{ width: `${percentComplete}%` }} />
-      </div>
-      <h4 className='block text-md font-medium' ref={timerRef} data-time={time}>
-        {formattedMsOrSeconds(time)}
-      </h4>
     </>
   )
 }
