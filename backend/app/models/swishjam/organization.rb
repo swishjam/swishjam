5fdce94fde71d683bd5d187043281ebda48d5935
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-08-16 12:42:39 -0700

    fixes bad `Swishjam::Organization` -> `Analytics::Session` association, adds `count` and `comparison_count` to /api/v1/sessions/timeseries endpoint

--- backend/app/models/swishjam/organization.rb
+++ backend/app/models/swishjam/organization.rb
@@ -21,8 +21,7 @@ module Swishjam
     has_many :analytics_sessions, 
               through: :analytics_devices, 
               class_name: Analytics::Session.to_s, 
-              foreign_key: :swishjam_organization_id, 
-              source: :swishjam_organization, 
+              source: :sessions, 
               dependent: :destroy
     has_many :analytics_billing_data_snapshots, 
               class_name: Analytics::BillingDataSnapshot.to_s, 
