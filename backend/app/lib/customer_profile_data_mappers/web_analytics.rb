1d5b651c0bc7e032d9890c2b1dcc7a41bb753931
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-08-17 16:56:23 -0700

    removes superfluous return statements

--- backend/app/lib/customer_profile_data_mappers/web_analytics.rb
+++ backend/app/lib/customer_profile_data_mappers/web_analytics.rb
@@ -1,15 +1,15 @@
 module CustomerProfileDataMappers
   class WebAnalytics < Base
     def find_organization(unique_identifier: nil, name: nil, url: nil)
-      return find_organization_by_unique_identifier(unique_identifier) ||
-              find_organization_by_name(name) ||
-              find_organization_by_domain(url)
+      find_organization_by_unique_identifier(unique_identifier) ||
+        find_organization_by_name(name) ||
+        find_organization_by_domain(url)
     end
 
     def find_user(unique_identifier: nil, email: nil, full_name: nil)
-      return find_user_by_unique_identifier(unique_identifier) ||
-              find_user_by_email(email) ||
-              find_user_by_full_name(full_name)
+      find_user_by_unique_identifier(unique_identifier) ||
+        find_user_by_email(email) ||
+        find_user_by_full_name(full_name)
     end
 
     private
