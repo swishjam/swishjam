c3ba43569750ee89cd07bb204ec38290db768976
Zach Zimbler <zach@tagsafe.io>
2023-08-18 10:18:40 -0600

    BarList component

--- dashboard/src/app/site-metrics/page.js
+++ dashboard/src/app/site-metrics/page.js
@@ -5,8 +5,8 @@ import { API } from '@/lib/api-client/base';
 import AuthenticatedView from '@/components/Auth/AuthenticatedView';
 import LineChartWithValue from '@/components/DashboardComponents/LineChartWithValue';
 // import ItemizedList from '@/components/DashboardComponents/ItemizedList';
-import ItemizedUsersList from '@/components/DashboardComponents/Prebuilt/ItemizedUsersList';
-import ItemizedOrganizationsList from '@/components/DashboardComponents/Prebuilt/ItemizedOrganizationList';
+import BarListCard from '@/components/DashboardComponents/BarListCard';
+import { BoltSlashIcon } from "@heroicons/react/24/outline";
 
 function FormattedLineChartData(name) {
   return {
@@ -21,7 +21,7 @@ const LoadingState = () => (
   <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-8">
     <div className='grid grid-cols-2 mt-8 flex items-center'>
       <div>
-        <h1 className="text-lg font-medium text-gray-700 mb-0">Dashboard</h1>
+        <h1 className="text-lg font-medium text-gray-700 mb-0">Marketing Site Metrics</h1>
       </div>
 
       <div className="w-full flex items-center justify-end">
@@ -29,54 +29,49 @@ const LoadingState = () => (
     </div>
     <div className='grid grid-cols-3 gap-6 pt-8'>
       <LineChartWithValue
-        key='MRR'
-        title='MRR'
-        value={null}
-        valueChange={null}
-        timeseries={null}
-      />
-      <LineChartWithValue
         key='subs'
-        title='Active Subscriptions'
+        title='User Sessions'
         value={null}
         valueChange={null}
         timeseries={null}
       />
     </div>
     <div className='grid grid-cols-2 gap-6 pt-8'>
-      {/* TODO: does this still try to fetch data? we just want it to stay in a loading state... */}
-      <ItemizedUsersList loadingStateOnly={true} /> 
-      <ItemizedOrganizationsList loadingStateOnly={true} />
     </div>
   </main>
 )
 
 const Home = () => {
-  const [mrrChart, setMrrChart] = useState(FormattedLineChartData('MRR'));
-  const [activeSubsChart, setActiveSubsChart] = useState(FormattedLineChartData('Active Subscriptions'));
   const [sessionsChart, setSessionsChart] = useState(FormattedLineChartData('User Sessions'));
+  const items = [
+    {
+      name: '/home',
+      href: '/',
+      value: 345,
+      icon: '',
+    },
+    {
+      name: '/poop',
+      href: '',
+      value: 3654,
+      icon: BoltSlashIcon,
+    },
+    {
+      name: '/poop1',
+      href: '',
+      value: 654,
+      icon: '',
+    },
+    {
+      name: '/poop-the-remix',
+      href: '',
+      value: 54,
+      icon: '',
+    }
+  ];
+
 
   const getData = async () => {
-    API.get('/api/v1/billing_data_snapshots').then(paymentData => {
-      console.log('paymentData', paymentData) 
-      setMrrChart({
-        ...mrrChart,
-        value: paymentData.current_mrr,
-        previousValue: paymentData.comparison_mrr,
-        previousValueDate: paymentData.comparison_end_time,
-        valueChange: paymentData.change_in_mrr,
-        timeseries: paymentData.current_mrr_timeseries.map((timeseries, index) => ({ ...timeseries, index, comparisonDate: paymentData.comparison_mrr_timeseries[index].date, comparisonValue: paymentData.comparison_mrr_timeseries[index].value})),
-      })
-
-      setActiveSubsChart({
-        ...activeSubsChart,
-        value: paymentData.current_num_active_subscriptions,
-        previousValue: paymentData.comparison_num_active_subscriptions,
-        previousValueDate: paymentData.comparison_end_time,
-        valueChange: paymentData.change_in_num_active_subscriptions,
-        timeseries: paymentData.current_num_active_subscriptions_timeseries
-      })
-    });
     
     API.get('/api/v1/sessions/timeseries').then((sessionData) => {
       console.log('Session Data', sessionData)
@@ -99,31 +94,24 @@ const Home = () => {
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-8">
       <div className='grid grid-cols-2 mt-8 flex items-center'>
         <div>
-          <h1 className="text-lg font-medium text-gray-700 mb-0">Dashboard</h1>
+          <h1 className="text-lg font-medium text-gray-700 mb-0">Marketing Site Metrics</h1>
         </div>
 
         <div className="w-full flex items-center justify-end">
         </div>
       </div>
       <div className='grid grid-cols-3 gap-6 pt-8'>
-        <LineChartWithValue
-          key={mrrChart.title}
-          title={mrrChart.title}
-          value={mrrChart.value}
-          previousValue={mrrChart.previousValue}
-          previousValueDate={mrrChart.previousValueDate}
-          timeseries={mrrChart.timeseries}
-          formatter={mrr => (mrr/100).toLocaleString('en-US', { style: "currency", currency: "USD" })}
-        />
-        <LineChartWithValue
-          key={activeSubsChart.title}
-          title={activeSubsChart.title}
-          value={activeSubsChart.value}
-          previousValue={mrrChart.previousValue}
-          previousValueDate={mrrChart.previousValueDate}
-          timeseries={activeSubsChart.timeseries}
-          formatter={numSubs => numSubs.toLocaleString('en-US')}
-        />
+        <div className='col-span-2'>
+          <LineChartWithValue
+            key={sessionsChart.title}
+            title={sessionsChart.title}
+            value={sessionsChart.value}
+            previousValue={sessionsChart.previousValue}
+            previousValueDate={sessionsChart.previousValueDate}
+            timeseries={sessionsChart.timeseries}
+            formatter={numSubs => numSubs.toLocaleString('en-US')}
+          />
+        </div>
         <LineChartWithValue
           key={sessionsChart.title}
           title={sessionsChart.title}
@@ -135,8 +123,29 @@ const Home = () => {
         />
       </div> 
       <div className='grid grid-cols-2 gap-6 pt-8'>
-        <ItemizedUsersList />
-        <ItemizedOrganizationsList />
+        <BarListCard
+          title={'Referrers'}
+          items={items} 
+        /> 
+        <BarListCard
+          title={'Top Pages'}
+          items={items} 
+        /> 
+      </div>
+      <div className='grid grid-cols-3 gap-6 pt-8'>
+        <BarListCard
+          title={'Devices'}
+          items={items} 
+        /> 
+        <BarListCard
+          title={'Browsers'}
+          items={items} 
+        />
+        <BarListCard
+          title={'Countries'}
+          items={items} 
+        /> 
+
       </div>
     </main>
   );
---
+++ dashboard/src/components/DashboardComponents/BarListCard.js
@@ -0,0 +1,18 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import Link from "next/link"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import BarListNoCard from "./BarListNoCard"
+
+export default function BarListCard({ title, items = [] }) {
+  
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-4">
+        <CardTitle className="text-sm font-medium cursor-default">{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <BarListNoCard items={items}/>
+      </CardContent>
+    </Card>
+  )
+}
