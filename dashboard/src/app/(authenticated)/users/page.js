576545ad92fa1e17b34eb4661f94f4416a9b61ff
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-09-25 18:05:34 -0400

    empty state for /users and /organizations

--- dashboard/src/app/(authenticated)/users/page.js
+++ dashboard/src/app/(authenticated)/users/page.js
@@ -77,11 +77,11 @@ const LoadingState = () => (
   </main>
 )
 
-const Users = ({ }) => {
-  const [usersData, setUsersData] = useState();
+export default function Users() {
   const router = useRouter();
+  const [usersData, setUsersData] = useState();
 
-  const handleClick = (id) => {
+  const navigateToUsersProfile = id => {
     router.push(`/users/${id}`);
   };
 
@@ -124,33 +124,40 @@ const Users = ({ }) => {
                       </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200 bg-white">
-                      {usersData.map((user) => (
-                        <tr
-                          key={user.email}
-                          className="group hover:bg-gray-50 duration-300 transition cursor-pointer"
-                          onClick={() => handleClick(user.id)}
-                        >
-                          <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm sm:pl-6 lg:pl-8">
-                            <div className="flex items-center">
-                              <div className="flex-shrink-0">
-                                <Avatar>
-                                  <AvatarImage src={user.image} />
-                                  <AvatarFallback>{user.initials}</AvatarFallback>
-                                </Avatar>
-                              </div>
-                              <div className="ml-4">
-                                <div className="font-medium text-gray-900">{user.full_name}</div>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-500">{user.email}</td>
-                          <td className="relative whitespace-nowrap py-3 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
-                            <Link href={`/users/${user.id}`} className="text-swishjam hover:text-swishjam-dark duration-300 transition">
-                              View<span className="sr-only">, {user.full_name}</span>
-                            </Link>
-                          </td>
-                        </tr>
-                      ))}
+                      {usersData.length === 0 
+                        ? (
+                          <div className='text-sm text-gray-500 text-center'>
+                            No users identified yet Once you begin to identify users in your app, they will show up here.
+                          </div>
+                        ) : (
+                          usersData.map((user) => (
+                            <tr
+                              key={user.email}
+                              className="group hover:bg-gray-50 duration-300 transition cursor-pointer"
+                              onClick={() => navigateToUsersProfile(user.id)}
+                            >
+                              <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm sm:pl-6 lg:pl-8">
+                                <div className="flex items-center">
+                                  <div className="flex-shrink-0">
+                                    <Avatar>
+                                      <AvatarImage src={user.image} />
+                                      <AvatarFallback>{user.initials}</AvatarFallback>
+                                    </Avatar>
+                                  </div>
+                                  <div className="ml-4">
+                                    <div className="font-medium text-gray-900">{user.full_name}</div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-500">{user.email}</td>
+                              <td className="relative whitespace-nowrap py-3 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
+                                <Link href={`/users/${user.id}`} className="text-swishjam hover:text-swishjam-dark duration-300 transition">
+                                  View<span className="sr-only">, {user.full_name}</span>
+                                </Link>
+                              </td>
+                            </tr>
+                          ))
+                        )}
                     </tbody>
                   </table>
                 </div>
@@ -163,5 +170,3 @@ const Users = ({ }) => {
     ) : <LoadingState />
   )
 }
-
-export default Users;
