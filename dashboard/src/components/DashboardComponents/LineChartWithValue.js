90f1cf75270e1b2db8d73f1b019c2827664526db
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-09-26 13:00:03 -0400

    make comparison data togglable in line chart

--- dashboard/src/components/DashboardComponents/LineChartWithValue.js
+++ dashboard/src/components/DashboardComponents/LineChartWithValue.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { AreaChart, Area, LineChart, Tooltip, Line, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { AreaChart, Area, Tooltip, ResponsiveContainer, XAxis, CartesianGrid } from 'recharts';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { HoverCard, HoverCardTrigger, HoverCardContent } from "@/components/ui/hover-card";
@@ -23,7 +23,7 @@ const LoadingState = ({ title }) => (
   </Card>
 )
 
-const CustomTooltip = ({ active, payload, label, valueFormatter, dateFormatter }) => {
+const CustomTooltip = ({ active, payload, label, valueFormatter, dateFormatter, includeComparison }) => {
   if (active && payload && payload.length) {
     let data = payload[0].payload; 
     
@@ -32,14 +32,14 @@ const CustomTooltip = ({ active, payload, label, valueFormatter, dateFormatter }
         <CardContent className="py-2">
           <div className="flex space-x-4 text-sm text-muted-foreground">
             <div className="flex items-center">
-              <CircleIcon className="mr-1 h-3 w-3 fill-swishjam text-swishjam" />
+              <CircleIcon className="mr-1 h-3 w-3" style={{ color: '#7dd3fc', fill: '#F2FAFE' }} />
               {valueFormatter(data.value)} - {dateFormatter(data.date)}
             </div>
           </div>
-          {data.comparisonValue >= 0 && 
+          {includeComparison && data.comparisonValue >= 0 && 
             <div className="flex space-x-4 text-sm text-muted-foreground">
               <div className="flex items-center">
-                <CircleIcon className="mr-1 h-3 w-3 fill-slate-200 text-slate-200" />
+                <CircleIcon className="mr-1 h-3 w-3" style={{ color: '#878b90', fill: '#E2E8F0' }} />
                 {valueFormatter(data.comparisonValue)} - {dateFormatter(data.comparisonDate)}
               </div>
             </div>
@@ -51,6 +51,64 @@ const CustomTooltip = ({ active, payload, label, valueFormatter, dateFormatter }
   return null;
 }
 
+const SettingsDropdown = ({ includeComparisonLine, showXAxis, showYAxis, onXAxisChange, onYAxisChange, onIncludeComparisonChange, onSettingChange }) => (
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Cog8ToothIcon className={`group-active:opacity-100 group-focus:opacity-100 group-hover:opacity-100 ring-0 opacity-0 duration-500 transition h-5 w-5 text-gray-500 cursor-pointer`} />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem
+        className={`cursor-pointer ${showXAxis ? 'text-swishjam font-medium hover:text-swishjam' : ''}`}
+        onClick={() => {
+          const valueChangedFrom = showXAxis;
+          const valueChangedTo = !showXAxis;
+          onXAxisChange(valueChangedTo)
+          onSettingChange({
+            attribute: 'show-y-axis',
+            valueWas: valueChangedFrom,
+            value: valueChangedTo
+          })
+        }}
+      >
+        {showXAxis ? <CheckCircleIcon className='h-4 w-4 absolute' /> : <CircleIcon className='h-4 w-4 absolute' />}
+        <span className='mx-6'>Show X-Axis</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem
+        className={`cursor-pointer ${showYAxis ? 'text-swishjam font-medium hover:text-swishjam' : ''}`}
+        onClick={() => {
+          const valueChangedFrom = showYAxis;
+          const valueChangedTo = !showYAxis;
+          onYAxisChange(valueChangedTo)
+          onSettingChange({
+            attribute: 'show-x-axis',
+            valueWas: valueChangedFrom,
+            value: valueChangedTo
+          })
+        }}
+      >
+        {showYAxis ? <CheckCircleIcon className='h-4 w-4 absolute' /> : <CircleIcon className='h-4 w-4 absolute' />}
+        <span className='ml-6'>Show Y-Axis</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem
+        className={`cursor-pointer ${includeComparisonLine ? 'text-swishjam font-medium hover:text-swishjam' : ''}`}
+        onClick={() => {
+          const valueChangedFrom = includeComparisonLine;
+          const valueChangedTo = !includeComparisonLine;
+          onIncludeComparisonChange(valueChangedTo)
+          onSettingChange({
+            attribute: 'include-comparison',
+            valueWas: valueChangedFrom,
+            value: valueChangedTo
+          })
+        }}
+      >
+        {includeComparisonLine ? <CheckCircleIcon className='h-4 w-4 absolute' /> : <CircleIcon className='h-4 w-4 absolute' />}
+        <span className='ml-6'>Include Comparison Data</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+) 
+
 export default function LineChartWithValue({ 
   title, 
   value, 
@@ -60,12 +118,14 @@ export default function LineChartWithValue({
   valueFormatter = val => val,
   dateFormatter = date => new Date(date).toLocaleDateString('en-us', { month: "2-digit", day: "2-digit" }),
   noDataMessage = 'No data available.',
+  includeComparisonData = true,
   includeSettingsDropdown = true,
   onSettingChange = () => {},
   showAxis = false
 }) {
   const [showXAxis, setShowXAxis] = useState(showAxis);
   const [showYAxis, setShowYAxis] = useState(showAxis);
+  const [includeComparisonLine, setIncludeComparisonLine] = useState(includeComparisonData);
   if ([null, undefined].includes(value) || [null, undefined].includes(timeseries)) return <LoadingState title={title} />;
   
   const changeInValue = typeof previousValue !== 'undefined' ? value - previousValue : null;
@@ -75,45 +135,15 @@ export default function LineChartWithValue({
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium cursor-default">{title}</CardTitle>
         {includeSettingsDropdown && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Cog8ToothIcon className={`group-active:opacity-100 group-focus:opacity-100 group-hover:opacity-100 ring-0 opacity-0 duration-500 transition h-5 w-5 text-gray-500 cursor-pointer`} />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem 
-                className={`cursor-pointer ${showXAxis ? 'text-swishjam font-medium hover:text-swishjam' : ''}`}
-                onClick={() => {
-                  const valueChangedFrom = showXAxis;
-                  const valueChangedTo = !showXAxis;
-                  setShowXAxis(valueChangedTo)
-                  onSettingChange({
-                    attribute: 'show-y-axis',
-                    valueWas: valueChangedFrom,
-                    value: valueChangedTo
-                  })
-                }}
-              >
-                {showXAxis ? <CheckCircleIcon className='h-4 w-4 absolute' />:<CircleIcon className='h-4 w-4 absolute' />}
-                <span className='mx-6'>Show X-Axis</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem 
-                className={`cursor-pointer ${showYAxis ? 'text-swishjam font-medium hover:text-swishjam' : ''}`}
-                onClick={() => {
-                  const valueChangedFrom = showYAxis;
-                  const valueChangedTo = !showYAxis;
-                  setShowYAxis(valueChangedTo)
-                  onSettingChange({
-                    attribute: 'show-x-axis',
-                    valueWas: valueChangedFrom,
-                    value: valueChangedTo
-                  })
-                }}
-              >
-                {showYAxis ? <CheckCircleIcon className='h-4 w-4 absolute' />:<CircleIcon className='h-4 w-4 absolute' />}
-                <span className='ml-6'>Show Y-Axis</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <SettingsDropdown 
+            includeComparisonLine={includeComparisonLine} 
+            showXAxis={showXAxis}
+            showYAxis={showYAxis}
+            onYAxisChange={setShowYAxis}
+            onXAxisChange={setShowXAxis}
+            onIncludeComparisonChange={setIncludeComparisonLine}
+            onSettingChange={onSettingChange} 
+          />
         )}
       </CardHeader>
       <CardContent>
@@ -157,21 +187,23 @@ export default function LineChartWithValue({
                   animationBegin={200}
                   animationDuration={400}
                   wrapperStyle={{ outline: "none" }}
-                  content={<CustomTooltip valueFormatter={valueFormatter} dateFormatter={dateFormatter} />}
+                  content={<CustomTooltip valueFormatter={valueFormatter} dateFormatter={dateFormatter} includeComparison={includeComparisonLine} />}
                   allowEscapeViewBox={{x: false, y: true}}
                   animationEasing='ease-in-out'
                 />
-                <Area
-                  type="monotone"
-                  dataKey='comparisonValue'
-                  stroke="#878b90"
-                  dot={{ r: 0 }}
-                  activeDot={{ r: 2 }}
-                  strokeWidth={2}
-                  fill="#E2E8F0"
-                  // opacity={0.2}
-                  // strokeDasharray="4 4"
-                />
+                {includeComparisonLine  && (
+                  <Area
+                    type="monotone"
+                    dataKey='comparisonValue'
+                    stroke="#878b90"
+                    dot={{ r: 0 }}
+                    activeDot={{ r: 2 }}
+                    strokeWidth={2}
+                    fill="#E2E8F0"
+                    // opacity={0.2}
+                    // strokeDasharray="4 4"
+                  />
+                )}
                 <Area
                   type="monotone"
                   dataKey='value'
