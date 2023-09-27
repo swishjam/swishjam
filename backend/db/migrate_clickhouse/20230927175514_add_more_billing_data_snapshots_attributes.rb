41523155d7207fb543359593796ba6dbc328816c
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-09-27 14:01:29 -0400

    add start_time_period and end_time_period to billing data snapshots

--- backend/db/migrate_clickhouse/20230927175514_add_more_billing_data_snapshots_attributes.rb
+++ backend/db/migrate_clickhouse/20230927175514_add_more_billing_data_snapshots_attributes.rb
@@ -13,5 +13,27 @@ class AddMoreBillingDataSnapshotsAttributes < ActiveRecord::Migration[6.1]
     add_column :billing_data_snapshots, :upgraded_mrr_amount_in_cents_for_time_period, :Int32
     add_column :billing_data_snapshots, :downgraded_mrr_amount_in_cents_for_time_period, :Int32
     add_column :billing_data_snapshots, :churned_mrr_amount_in_cents_for_time_period, :Int32
+
+    add_column :billing_data_snapshots, :start_time_period, :DateTime
+    add_column :billing_data_snapshots, :end_time_period, :DateTime
+  end
+
+  def down
+    remove_column :billing_data_snapshots, :num_new_customers_for_time_period
+    remove_column :billing_data_snapshots, :num_new_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_new_paid_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_new_free_trial_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_downgraded_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_upgraded_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_canceled_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_canceled_paid_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_paused_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :num_resumed_subscriptions_for_time_period
+    remove_column :billing_data_snapshots, :upgraded_mrr_amount_in_cents_for_time_period
+    remove_column :billing_data_snapshots, :downgraded_mrr_amount_in_cents_for_time_period
+    remove_column :billing_data_snapshots, :churned_mrr_amount_in_cents_for_time_period
+
+    remove_column :billing_data_snapshots, :start_time_period
+    remove_column :billing_data_snapshots, :end_time_period
   end
 end
