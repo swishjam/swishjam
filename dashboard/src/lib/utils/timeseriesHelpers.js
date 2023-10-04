8e684e579b2ff96d8731555a4b0e2a4f75171d6b
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-04 16:12:15 -0700

    remove legacy timeseries helper

--- dashboard/src/lib/utils/timeseriesHelpers.js
+++ dashboard/src/lib/utils/timeseriesHelpers.js
@@ -1,15 +1,5 @@
 import { ONE_DAY_IN_MS, startOfDay, dateToUTC } from "./timeHelpers";
 
-const filledInTimeseries = ({ timeseries, groupedBy, expectedNumDataPoints = 5 }) => {
-  if (timeseries.length >= expectedNumDataPoints) return timeseries;
-  const secondIncrements = groupedBy === 'day' ? ONE_DAY_IN_MS : groupedBy === 'week' ? ONE_DAY_IN_MS * 7 : ONE_DAY_IN_MS * 30;
-  const expectedTimes = Array.from({ length: expectedNumDataPoints }, (_, i) => dateToUTC(startOfDay(new Date() - secondIncrements * i)).toISOString());
-  expectedTimes.forEach(time => {
-    if (!timeseries.find(datapoint => datapoint.date === time)) timeseries.push({ date: time });
-  })
-  return timeseries.sort((a, b) => new Date(a.date) - new Date(b.date));
-}
-
 const dateFormatterForGrouping = grouping => {
   switch(grouping) {
     case 'month':
@@ -25,4 +15,4 @@ const dateFormatterForGrouping = grouping => {
   }
 }
 
-export { filledInTimeseries, dateFormatterForGrouping }
+export { dateFormatterForGrouping }
