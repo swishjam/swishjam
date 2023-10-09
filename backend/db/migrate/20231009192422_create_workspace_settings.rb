66858ae275ac8742954cff2e5355cf940f27c344
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-09 15:54:15 -0700

    rake task for workspace settings migration

--- backend/db/migrate/20231009192422_create_workspace_settings.rb
+++ backend/db/migrate/20231009192422_create_workspace_settings.rb
@@ -1,14 +1,14 @@
 class CreateWorkspaceSettings < ActiveRecord::Migration[6.1]
-  def change
+  def up
+    return if table_exists?(:workspace_settings)
     create_table :workspace_settings, id: :uuid do |t|
       t.references :workspace, type: :uuid, null: false, foreign_key: true
       t.boolean :use_product_data_source_in_lieu_of_marketing
       t.boolean :use_marketing_data_source_in_lieu_of_product
       t.timestamps
     end
-    
-    Workspace.includes(:settings).where(settings: { id: nil }).each do |workspace|
-      WorkspaceSetting.generate_default_for(workspace)
-    end
+  end
+
+  def down
   end
 end
