bf7ad1af9ee81f9c2b7517d2b87fa25f02be44af
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-12 10:29:53 -0700

    improved serializer

--- backend/app/serializers/workspace_invitation_serializer.rb
+++ backend/app/serializers/workspace_invitation_serializer.rb
@@ -2,10 +2,10 @@ class WorkspaceInvitationSerializer < ActiveModel::Serializer
   attributes :id, :invite_token, :invited_email, :expires_at, :accepted_at, :invited_by_user, :workspace
 
   def invited_by_user
-    { email: object.invited_by_user.email }
+    { id: object.invited_by_user.id,  email: object.invited_by_user.email }
   end
 
   def workspace
-    { name: object.workspace.name }
+    { id: object.workspace.id,  name: object.workspace.name }
   end
 end
