5de924a7a30adf64e2b6051b79a53a1a2e633c42
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-16 12:33:46 -0700

    moves Resend event object to strings

--- backend/app/controllers/api/v1/webhooks/resend_controller.rb
+++ backend/app/controllers/api/v1/webhooks/resend_controller.rb
@@ -7,19 +7,19 @@ module Api
           payload = JSON.parse(request.body.read)
           public_key = workspace.api_keys.for_data_source!(ApiKey::ReservedDataSources.RESEND).public_key
           swishjam_obj = {
-            uuid: payload.dig('data', 'email_id'),
-            event: payload['type'],
-            timestamp: DateTime.parse(payload.dig('data', 'created_at')).to_i * 1_000,
-            source: 'resend',
-            from: payload['from'],
-            to: (payload['to'] || []).join(', '),
-            subject: payload['subject'],
+            'uuid' => payload.dig('data', 'email_id'),
+            'event' => payload['type'],
+            'timestamp' => DateTime.parse(payload.dig('data', 'created_at')).to_i * 1_000,
+            'source' => 'resend',
+            'from' => payload['from'],
+            'to' => (payload['to'] || []).join(', '),
+            'subject' => payload['subject'],
           }
           if payload['type'] == 'email.clicked'
-            swishjam_obj[:click_ip_address] = payload.dig('click', 'ipAddress')
-            swishjam_obj[:click_link] = payload.dig('click', 'link')
-            swishjam_obj[:click_timestamp] = payload.dig('click', 'timestamp')
-            swishjam_obj[:click_user_agent] = payload.dig('click', 'userAgent')
+            swishjam_obj['click_ip_address'] = payload.dig('click', 'ipAddress')
+            swishjam_obj['click_link'] = payload.dig('click', 'link')
+            swishjam_obj['click_timestamp'] = payload.dig('click', 'timestamp')
+            swishjam_obj['click_user_agent'] = payload.dig('click', 'userAgent')
           end
           CaptureAnalyticDataJob.perform_async(public_key, [swishjam_obj], nil)
         end
