7add96ef79b0e777e493560e896faad48cf4b25b
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-17 10:31:26 -0700

    hooks in user profile with real enrichment data

--- backend/app/serializers/user_profile_serializer.rb
+++ backend/app/serializers/user_profile_serializer.rb
@@ -1,7 +1,11 @@
 class UserProfileSerializer < ActiveModel::Serializer
-  attributes :id, :email, :initials, :full_name, :first_name, :last_name, :metadata, :created_at, :organizations, :enrichment_data
+  attributes :id, :user_unique_identifier, :email, :initials, :full_name, :first_name, :last_name, :metadata, :created_at, :organizations, :enrichment_data
 
   def organizations
     object.analytics_organization_profiles
+  end
+
+  def enrichment_data
+    object.enrichment_data || {}
   end
 end
