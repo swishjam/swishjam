386c84ab2b6bd70ed0308c361467d0d2062bb2e3
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-10-22 13:27:48 -0700

    starts handling of identify/organization events

--- backend/app/jobs/ingest_analytics_queue_job.rb
+++ backend/app/jobs/ingest_analytics_queue_job.rb
@@ -7,9 +7,24 @@ class IngestAnalyticsQueueJob
     events = Ingestion::QueueManager.pop_all_records_from_queue(Ingestion::QueueManager::Queues.EVENTS)
     ingestion.num_records = events.count
     begin
-      formatted_events = events.map{ |e| JSON.parse(e) }
-      Analytics::Event.insert_all!(events)
-      # TODO: need to push identify and organization events into a new queue to be processed later.
+      identify_events = []
+      organization_events = []
+      formatted_events = events.map do |e| 
+        json_event = JSON.parse(e)
+        if json_event['event'] == 'identify'
+          identify_events << e
+        elsif json_event['event'] == 'organization'
+          organization_events << e
+        end
+        json_event
+      end
+      Analytics::Event.insert_all!(formatted_events)
+      if identify_events.count > 0
+        Ingestion::QueueManager.push_records_into_queue(Ingestion::QueueManager::Queues.IDENTIFY, identify_events)
+      end
+      if organization_events.count > 0
+        Ingestion::QueueManager.push_records_into_queue(Ingestion::QueueManager::Queues.ORGANIZATION, organization_events)
+      end
       ingestion.completed_at = Time.current
       ingestion.num_seconds_to_complete = ingestion.completed_at - ingestion.started_at
       ingestion.save!
