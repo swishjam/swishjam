eca3fdae8b7b0e84a425a07b55cfd2f18a61ae4c
Zach Zimbler <zachzimbler@gmail.com>
2023-10-31 16:41:28 -0600

    Slightly tweaked the console log on the sdk for disabled hosts

--- sdk/client/core/src/requester.mjs
+++ sdk/client/core/src/requester.mjs
@@ -9,7 +9,7 @@ export class Requester {
 
   async send(data, onError = async () => { }) {
     if (!this._shouldSendRequest()) {
-      console.log(`%cSwishjam is in development mode while in localhost and won't send data to the server`, `color: #7487F7; font-weight: bold;`)  
+      console.log(`%cThis URL is disabled! Swishjam is in development mode while in localhost and won't send data to the server`, `color: #7487F7; font-weight: bold;`)  
       console.log(`%cData Captured By Swishjam`, `color: #7487F7; font-weight: bold;`)  
       console.log(`%O`, data)  
       return
