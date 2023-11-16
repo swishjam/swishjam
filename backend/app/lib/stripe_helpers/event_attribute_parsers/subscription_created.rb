4efea30c581458ce197d9c27dff4e0f4562f7d56
Zach Zimbler <zachzimbler@gmail.com>
2023-11-16 12:00:27 -0700

    added ids to the stripe json captured

--- backend/app/lib/stripe_helpers/event_attribute_parsers/subscription_created.rb
+++ backend/app/lib/stripe_helpers/event_attribute_parsers/subscription_created.rb
@@ -1,7 +1,7 @@
 module StripeHelpers
   module EventAttributeParsers
     class SubscriptionCreated < Base
-      self.attributes_to_capture = [:'amount', :'customer', :'items']
+      self.attributes_to_capture = [:'id', :'customer']
     end
   end
 end
