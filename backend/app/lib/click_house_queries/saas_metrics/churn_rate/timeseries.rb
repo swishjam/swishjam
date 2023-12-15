8185ad9591c0b279234b5a1e8c0ca1a205e31756
Collin Schneider <collin.thomas.schneider@gmail.com>
2023-12-14 18:43:48 -0800

    mf churn rate!!

--- backend/app/lib/click_house_queries/saas_metrics/churn_rate/timeseries.rb
+++ backend/app/lib/click_house_queries/saas_metrics/churn_rate/timeseries.rb
@@ -5,11 +5,14 @@ module ClickHouseQueries
         include ClickHouseQueries::Helpers
         include TimeseriesHelper
 
-        def initialize(public_keys, start_time: 6.months.ago, end_time: Time.current, percentage_precision: 3, num_days_in_churn_period: 30)
+        def initialize(public_keys, start_time: 30.days.ago, end_time: Time.current, num_days_in_churn_period: 30)
           @public_keys = public_keys.is_a?(Array) ? public_keys : [public_keys]
           @group_by = derived_group_by(start_ts: start_time, end_ts: end_time)
-          @snapshot_start_time, @snapshot_end_time = rounded_timestamps(start_ts: start_time - 30.days, end_ts: end_time - 30.days, group_by: @group_by)
-          @percentage_precision = percentage_precision
+          @snapshot_start_time, @snapshot_end_time = rounded_timestamps(
+            start_ts: start_time - num_days_in_churn_period.days, 
+            end_ts: end_time - num_days_in_churn_period.days, 
+            group_by: @group_by
+          )
           @num_days_in_churn_period = num_days_in_churn_period
         end
 
@@ -18,32 +21,23 @@ module ClickHouseQueries
         end
 
         def sql
-          THIS IS SO CLOSE! CLICKHOUSE DOESNT SUPPORT NON EQUI JOIN CONDITIONS
-          MAYBE WE CAN NORMALIZE THE DATES SOMEHOW TO EXTRACT THE MONTH OR USE SOME KIND OF MODULE OPERATOR?
           <<~SQL
-            SELECT
-              bds.snapshot_date + INTERVAL #{@num_days_in_churn_period} DAY AS churn_date,
-              ROUND(CASE 
-                WHEN MAX(bds.num_customers_with_paid_subscriptions) > 0 THEN
-                  SUM(CASE WHEN e.uuid IS NULL OR e.uuid = '' THEN 0 ELSE 1 END) * 100.0 / MAX(bds.num_customers_with_paid_subscriptions)
-                ELSE 0
-              END, #{@percentage_precision}) AS churn_rate_percentage
-            FROM (
-              SELECT
-                DATE_TRUNC('day', captured_at) AS snapshot_date,
-                MAX(num_customers_with_paid_subscriptions) AS num_customers_with_paid_subscriptions
-              FROM billing_data_snapshots
-              WHERE 
-                swishjam_api_key IN #{formatted_in_clause(@public_keys)} AND
-                captured_at BETWEEN '#{formatted_time(@snapshot_start_time)}' AND '#{formatted_time(@snapshot_end_time)}'
-              GROUP BY snapshot_date
-            ) AS bds
-            LEFT JOIN events AS e ON e.occurred_at >= bds.snapshot_date 
-              AND e.occurred_at <= bds.snapshot_date + INTERVAL #{@num_days_in_churn_period} DAY
-              AND e.name = 'stripe.supplemental.customer.churned' 
-              AND e.swishjam_api_key IN #{formatted_in_clause(@public_keys)}
-            GROUP BY churn_date
-            ORDER BY churn_date ASC
+            SELECT 
+              snapshots.captured_at AS snapshot_date,
+              snapshots.num_customers_with_paid_subscriptions AS num_customers_with_paid_subscriptions_at_snapshot_date,
+              snapshots.captured_at + INTERVAL #{@num_days_in_churn_period} DAY AS churn_period_end_date,
+              CAST(COUNT(churn_events.occurred_at) AS INT) AS num_churned_customers_in_period,
+              CAST(ROUND(COUNT(churn_events.occurred_at) / snapshots.num_customers_with_paid_subscriptions * 100, 2) AS FLOAT) AS churn_rate
+            FROM billing_data_snapshots AS snapshots
+            LEFT JOIN events AS churn_events ON snapshots.swishjam_api_key = churn_events.swishjam_api_key
+            WHERE 
+              snapshots.swishjam_api_key IN #{formatted_in_clause(@public_keys)} AND
+              churn_events.name = 'stripe.supplemental.customer.churned' AND 
+              churn_events.occurred_at >= snapshots.captured_at AND 
+              churn_events.occurred_at <= snapshots.captured_at + INTERVAL #{@num_days_in_churn_period} DAY AND
+              snapshots.captured_at BETWEEN '#{formatted_time(@snapshot_start_time)}' AND '#{formatted_time(@snapshot_end_time)}'
+            GROUP BY snapshot_date, num_customers_with_paid_subscriptions_at_snapshot_date
+            ORDER BY snapshot_date
           SQL
         end
       end
--- backend/spec/lib/click_house_queries/saas_metrics/churn_rate/timeseries_spec.rb
+++ backend/spec/lib/click_house_queries/saas_metrics/churn_rate/timeseries_spec.rb
@@ -4,20 +4,39 @@ RSpec.describe ClickHouseQueries::SaasMetrics::ChurnRate::Timeseries do
   describe '#get' do
     it 'returns the churn rate timeseries for the given public keys' do
       my_api_key = 'xyz'
-      # 30.times do |i|
-      #   FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: 30.days.ago - i.day, num_customers_with_paid_subscriptions: 100 - i)
-      # end
-      # 30.times do |i|
-      #   rand(1..10).times do
-      #     FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: Time.current - i.days)
-      #   end
-      # end
-      churn_date = Time.current
-      FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: churn_date - 30.days, num_customers_with_paid_subscriptions: 105)
-      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: churn_date)
+      current_time = Time.current
+      # two days ago churn_period
+      FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: current_time - 32.days, num_customers_with_paid_subscriptions: 90)
+      # yesterdays churn_period
+      FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: current_time - 31.days, num_customers_with_paid_subscriptions: 100)
+      # todays churn_period
+      FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: current_time - 30.days, num_customers_with_paid_subscriptions: 110)
+      # tomorrows churn_period
+      FactoryBot.create(:analytics_billing_data_snapshot, swishjam_api_key: my_api_key, captured_at: current_time - 29.days, num_customers_with_paid_subscriptions: 120)
 
-      result = described_class.new(my_api_key).get
-      byebug
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: current_time)
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: current_time - 2.days)
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: current_time - 3.days)
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: current_time - 1.day)
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'stripe.supplemental.customer.churned', occurred_at: current_time + 1.minute)
+      FactoryBot.create(:analytics_event, swishjam_api_key: my_api_key, name: 'a_different_event', occurred_at: current_time - 3.days)
+      FactoryBot.create(:analytics_event, swishjam_api_key: 'someone_else', name: 'stripe.supplemental.customer.churned', occurred_at: current_time - 3.days)
+
+      results = described_class.new(my_api_key).get
+      todays_churn_period = results.find{ |r| r['churn_period_end_date'].to_datetime.to_s == current_time.to_datetime.to_s }
+      expect(todays_churn_period['num_churned_customers_in_period']).to eq(4)
+      expect(todays_churn_period['num_customers_with_paid_subscriptions_at_snapshot_date']).to eq(110)
+      expect(todays_churn_period['churn_rate']).to eq((4.0 / 110 * 100).round(2))
+
+      yesterdays_churn_period = results.find{ |r| r['churn_period_end_date'].to_datetime.to_s == (current_time - 1.day).to_datetime.to_s }
+      expect(yesterdays_churn_period['num_churned_customers_in_period']).to eq(3)
+      expect(yesterdays_churn_period['num_customers_with_paid_subscriptions_at_snapshot_date']).to eq(100)
+      expect(yesterdays_churn_period['churn_rate']).to eq((3.0 / 100 * 100).round(2))
+
+      two_days_ago_churn_period = results.find{ |r| r['churn_period_end_date'].to_datetime.to_s == (current_time - 2.days).to_datetime.to_s }
+      expect(two_days_ago_churn_period['num_churned_customers_in_period']).to eq(2)
+      expect(two_days_ago_churn_period['num_customers_with_paid_subscriptions_at_snapshot_date']).to eq(90)
+      expect(two_days_ago_churn_period['churn_rate']).to eq((2.0 / 90 * 100).round(2))
     end
   end
 end
