0e333915e02dbb35e2c443628f0de22b38b455f4
Zach Zimbler <zachzimbler@gmail.com>
2023-12-22 15:18:42 -0700

    Updated the front end to add reports customization

--- dashboard/src/components/Automations/Reports/AddReportModal.js
+++ dashboard/src/components/Automations/Reports/AddReportModal.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import SwishjamAPI from '@/lib/api-client/swishjam-api';
-import { LuPlus } from "react-icons/lu";
+import { LuPlus, LuTrash } from "react-icons/lu";
 import {
   Dialog,
   DialogContent,
@@ -26,45 +26,43 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { useForm } from "react-hook-form"
+import { useForm, useFieldArray } from "react-hook-form"
 import { toast } from 'sonner'
 import LoadingSpinner from '@components/LoadingSpinner';
 import SlackMessagePreview from '@/components/Slack/SlackMessagePreview';
 import MessageBodyMarkdownRenderer from '@/components/Slack/MessageBodyMarkdownRenderer';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
-
-const slackMessageHeader = '📅 10/2/2023 \n\n'
-const reportMarketingSection = '📣 **Marketing Site:** \n\n↔️ Sessions: 500\n\n📉 Unique Visitors: 340\n\n📈 Page Views: 456\n\n ';
-const reportProductSection = '**🧑‍💻 Product Usage:**\n\n↔️ Daily Active Users: 500\n\n📉 Sessions: 340\n\n📈 New Users: 456';
-const reportMarkdown = '📅 10/2/2023 \n\n📣 **Marketing Site:** \n\n↔️ Sessions: 500\n\n📉 Unique Visitors: 340\n\n📈 Page Views: 456\n\n **🧑‍💻 Product Usage:**\n\n↔️ Daily Active Users: 500\n\n📉 Sessions: 340\n\n📈 New Users: 456'
-
 export default function AddReportModal({ onNewReport, open, setOpen }) {
   const dialogRef = useRef();
-  const form = useForm();
+  const form = useForm({ defaultValues: { name:'', cadence:'daily', sending_mechanism:'slack', slack_channel:'', messageSections: [] }});
+  const fieldArray = useFieldArray({
+    control: form.control,
+    name: "messageSections",
+  }); 
+  
   const [loading, setLoading] = useState(false);
   const [slackChannels, setSlackChannels] = useState();
-  const [messageSections, setMessageSections] = useState([])
-
+  const [mkdPreview, setMkdPreview] = useState()
 
   const resetForm = () => {
-    setMessageSections([])
     form.reset();
   }
 
   async function onSubmit(values) {
+    
     setLoading(true)
-    if (!values.name || !values.slack_channel) {
+    if (!values.name || !values.slack_channel || values.messageSections.length == 0) {
       toast.error('All fields are required')
       setLoading(false);
       return;
     }
     const { report, error } = await SwishjamAPI.Reports.create({
       name: values.name,
-      cadence: 'daily',
-      sending_mechanism: 'slack',
+      cadence: values.cadence,
+      sending_mechanism: values.sending_mechanism,
       config: {
-        sections: messageSections,
+        sections: values.messageSections,
         slack_channel_id: values.slack_channel
       }
     })
@@ -75,7 +73,6 @@ export default function AddReportModal({ onNewReport, open, setOpen }) {
     } else {
       toast.success(`${values.name} created successfully..`)
       form.reset();
-      setMessageSections([])
       dialogRef.current.click();
       onNewReport(report)
     }
@@ -95,6 +92,30 @@ export default function AddReportModal({ onNewReport, open, setOpen }) {
       setSlackChannels(sortedChannels);
     });
   }, [])
+
+  console.log(form.getValues())
+
+  const renderMarkdown = () => {
+    const slackMessageHeader = '📅 10/2/2023 \n\n'
+    const reportWebSection = '📣 **Marketing Site:** \n\n↔️ Sessions: 500\n\n📉 Unique Visitors: 340\n\n📈 Page Views: 456\n\n ';
+    const reportProductSection = '**🧑‍💻 Product Usage:**\n\n↔️ Daily Active Users: 500\n\n📉 Sessions: 340\n\n📈 New Users: 456\n\n';
+    const reportMarkdown = '📅 10/2/2023 \n\n📣 **Marketing Site:** \n\n↔️ Sessions: 500\n\n📉 Unique Visitors: 340\n\n📈 Page Views: 456\n\n **🧑‍💻 Product Usage:**\n\n↔️ Daily Active Users: 500\n\n📉 Sessions: 340\n\n📈 New Users: 456\n\n'
+    let msg = slackMessageHeader;
+    //slackMessageHeader
+  
+    form.getValues('messageSections').map((sec) => {
+      if(sec.type == 'web') {
+        msg += reportWebSection
+      }
+      if(sec.type == 'product') {
+        msg += reportProductSection 
+      }
+    })
+    
+    setMkdPreview(msg) 
+  }
+
+  form.watch((data, {name, type}) => renderMarkdown())
 
   return (
     <Dialog open={open} onOpenChange={(v) => {resetForm();setOpen(v)}}>
@@ -119,7 +140,7 @@ export default function AddReportModal({ onNewReport, open, setOpen }) {
             <ScrollArea className="max-h-96 overflow-y-scroll border border-gray-200 rounded-md">
               <SlackMessagePreview
                 header={'Daily Update'}
-                body={<MessageBodyMarkdownRenderer body={slackMessageHeader} />}
+                body={<MessageBodyMarkdownRenderer body={mkdPreview} />}
                 className={'mt-2 border-0'}
               />
             </ScrollArea>
@@ -217,52 +238,54 @@ export default function AddReportModal({ onNewReport, open, setOpen }) {
                 <div className='mt-4'>
                   <FormLabel>Report Sections</FormLabel>
 
-                  {messageSections.length == 0 &&
-                  <div 
-                    onClick={() => setMessageSections([{ type: 'web' }])}
-                    className='px-6 py-20 border-2 border-gray-200 border-dashed mt-2 rounded-md text-center text-gray-400 text-sm cursor-pointer duration-500 transition-all hover:border-swishjam hover:text-swishjam'
-                  >
-                    <LuPlus size="24" className='mx-auto mb-4'/>
-                    Add Report Sections 
-                  </div>}
-                  {messageSections.length > 0 &&
-                  <ul className='grid gap-y-2 mt-2'>
-                    {messageSections.map((section, index) => 
-                      <li key={index} className='w-full flex'>
-                        <FormField
-                          className="grow" 
-                          control={form.control}
-                          name={'message_section'+index}
-                          render={({ field }) => (
-                            <FormItem>
-                              <Select onValueChange={field.onChange}>
-                                <FormControl>
-                                  <SelectTrigger>
-                                    <SelectValue placeholder="Web Analytics" />
-                                  </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                  <SelectItem className="cursor-pointer" value="web">Web Analytics</SelectItem>
-                                  <SelectItem className="cursor-pointer" value="product">Product Analytics</SelectItem>
-                                </SelectContent>
-                              </Select>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />    
+                  {fieldArray.fields.length == 0 &&
+                    <div
+                      onClick={() => fieldArray.append({ type: 'web' })}
+                      className='px-6 py-20 border-2 border-gray-200 border-dashed mt-2 rounded-md text-center text-gray-400 text-sm cursor-pointer duration-500 transition-all hover:border-swishjam hover:text-swishjam'
+                    >
+                      <LuPlus size="24" className='mx-auto mb-4' />
+                      Add Report Sections
+                    </div>}
+                  {fieldArray.fields.length > 0 &&
+                    <ul className='grid gap-y-2 mt-2'>
+                      {fieldArray.fields.map((field, index) =>
+                        <li key={index} className='w-full flex'>
+                          <div className="flex-1" >
+                            <FormField
+                              key={field.id}
+                              control={field.control}
+                              name={`messageSections.${index}.type`}
+                              render={({ field }) => (
+                                <FormItem>
+                                  <Select value={field.value} key={field.id} onValueChange={field.onChange}>
+                                    <FormControl>
+                                      <SelectTrigger>
+                                        <SelectValue placeholder="Web Analytics" />
+                                      </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                      <SelectItem className="cursor-pointer" value="web">Web Analytics</SelectItem>
+                                      <SelectItem className="cursor-pointer" value="product">Product Analytics</SelectItem>
+                                    </SelectContent>
+                                  </Select>
+                                  <FormMessage />
+                                </FormItem>
+                              )}
+                            />    
+                        </div> 
                         <Button
-                          onClick={() => setMessageSections([...messageSections, { type: 'web' }])}
+                          onClick={() =>  fieldArray.remove(index)} 
                           type={'button'}
                           variant="ghost"
-                          className={`flex-none w-14 h-14`}
+                          className={`flex-none ml-2 duration-500 hover:text-rose-600`}
                         >
-                          <LuPlus size={12} />
+                          <LuTrash size={14} />
                         </Button>
                       </li> 
                     )}
                     <li key="add-more-button">
                       <Button
-                        onClick={() => setMessageSections([...messageSections, {type: 'web'}])} 
+                        onClick={() =>  fieldArray.append({ type: 'web' })} 
                         type={'button'}
                         variant="outline" 
                         className={`!mt-2 w-full`}
@@ -275,7 +298,7 @@ export default function AddReportModal({ onNewReport, open, setOpen }) {
                 <Button
                   className={`!mt-6 w-full flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 ${loading ? 'bg-swishjam-dark' : 'bg-swishjam'} hover:bg-swishjam-dark`}
                   type="submit"
-                  disabled={loading || messageSections.length === 0}
+                  disabled={loading || form.getValues('messageSections').length == 0 }
                 >
                   {loading ? <LoadingSpinner color="white" /> : 'Create Report'}
                 </Button>
