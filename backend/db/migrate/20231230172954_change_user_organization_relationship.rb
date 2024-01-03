06674f351ad561ebac4953b6486be9b12c218d3b
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-01-03 13:12:27 -0500

    drop old user -> organization join table

--- backend/db/migrate/20231230172954_change_user_organization_relationship.rb
+++ backend/db/migrate/20231230172954_change_user_organization_relationship.rb
@@ -1,6 +1,7 @@
 class ChangeUserOrganizationRelationship < ActiveRecord::Migration[6.1]
   def up
     drop_table :analytics_organization_members if table_exists? :analytics_organization_members
+    drop_table :analytics_organization_profiles_users if table_exists? :analytics_organization_profiles_users
     create_table :analytics_organization_members do |t|
       t.references :analytics_organization_profile, type: :uuid, null: false, index: { name: 'index_organization_profiles_users_on_org_unique_identifier' }
       t.references :analytics_user_profile, type: :uuid, null: false, index: { name: 'index_organization_profiles_users_on_user_unique_identifier' }
