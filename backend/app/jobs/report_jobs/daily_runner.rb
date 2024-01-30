a5c0b8cf2f3fb1f46e5880af30aecfb9b9aa7320
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-01-30 13:28:25 -0800

    fix bad report job class name

--- backend/app/jobs/report_jobs/daily_runner.rb
+++ backend/app/jobs/report_jobs/daily_runner.rb
@@ -1,10 +1,12 @@
-class DailyReportsBatchRunnerJob
-  include Sidekiq::Job
-  queue_as :default
+module ReportJobs
+  class DailyRunner
+    include Sidekiq::Job
+    queue_as :default
 
-  def perform
-    Report.enabled.daily_cadence.each do |report|
-      ReportJobs::SendReport.perform_async(report.id)
+    def perform
+      Report.enabled.daily_cadence.each do |report|
+        ReportJobs::SendReport.perform_async(report.id)
+      end
     end
   end
 end
