c308728a914f3f59067233bfefb96e6618504295
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-01-30 15:57:40 -0800

    adds log to boot

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.logger.info "Booting app in #{Rails.env} (has staging ENV flag? #{ENV['IS_STAGING']}) environment..."
+
 # Initialize the Rails application.
 Rails.application.initialize!
