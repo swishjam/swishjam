2ef538c7359ea64624a57f4d16de1165ba02956c
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-01-31 21:27:29 -0800

    update default slack channel params

--- backend/app/lib/slack/client.rb
+++ backend/app/lib/slack/client.rb
@@ -21,7 +21,7 @@ module Slack
       end.flatten
     end
 
-    def post_message_to_channel(channel:, text: nil, unfurl_links: nil, unfurl_media: nil, blocks: nil)
+    def post_message_to_channel(channel:, text: nil, blocks: nil, unfurl_links: true, unfurl_media: true)
       raise BadRequestError, "`post_message_to_channel` must contain either `text` or `blocks` argument." if text.blank? && blocks.blank?
       if !Rails.env.production? && ENV['ENABLE_SLACK_NOTIFICATIONS_IN_DEV'] != 'true'
         Rails.logger.info("\nWould have sent Slack message to channel #{channel} with text: #{text} and blocks: #{blocks}\n")
