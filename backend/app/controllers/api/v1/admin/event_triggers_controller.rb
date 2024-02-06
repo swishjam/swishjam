999f8d8f48a2cd0cc78882c6233a9b30710ab39c
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-02-06 07:34:15 -0800

    filter out odd negative `seconds_from_occurred_at_to_triggered` in admin panel

--- backend/app/controllers/api/v1/admin/event_triggers_controller.rb
+++ backend/app/controllers/api/v1/admin/event_triggers_controller.rb
@@ -3,7 +3,9 @@ module Api
     module Admin
       class EventTriggersController < BaseController
         def delay_time_timeseries
-          timeseries = TriggeredEventTrigger.group_by_minute(:created_at, time_zone: 'UTC', n: 15, range: 7.days.ago..Time.current).average(:seconds_from_occurred_at_to_triggered)
+          timeseries = TriggeredEventTrigger.where('seconds_from_occurred_at_to_triggered > 0')
+                                              .group_by_minute(:created_at, time_zone: 'UTC', n: 15, range: 7.days.ago..Time.current)
+                                              .average(:seconds_from_occurred_at_to_triggered)
           render json: timeseries, status: :ok
         end
       end
