762d9c1207972622d4c0a9e95d14d84ca03e6dd1
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-02-15 08:12:08 -0800

    apply stripe customer details to all stripe events when possible

--- backend/app/lib/stripe_helpers/event_attribute_parsers/base.rb
+++ backend/app/lib/stripe_helpers/event_attribute_parsers/base.rb
@@ -24,11 +24,11 @@ module StripeHelpers
           json[:amount] = stripe_object.amount
           json[:display_amount] = "$#{sprintf('%.2f', (stripe_object.amount / 100.0))}"
         end
+        apply_customer_details_to_properties(json)
         json
       end
 
       def event_properties
-        # Hash.new.tap do |h| 
         default_json_properties.tap do |h|
           (self.class.attributes_to_capture || []).each do |attribute|
             attribute_chain = attribute.to_s.split('.')
@@ -49,6 +49,22 @@ module StripeHelpers
             end
           end
         end
+      end
+
+      def apply_customer_details_to_properties(properties_json)
+        if stripe_object.respond_to?(:customer) && stripe_object.customer.present? && @stripe_event.respond_to?(:account) && @stripe_event.account.present?
+          stripe_customer = stripe_object.customer
+          if stripe_customer.is_a?(String)
+            stripe_customer = ::Stripe::Customer.retrieve(stripe_customer, stripe_account: @stripe_event.account)
+          end
+          properties_json[:customer_id] = stripe_customer.id
+          properties_json[:customer_email] = stripe_customer.email if stripe_customer.respond_to?(:email) && !stripe_customer.email.blank?
+          properties_json[:customer_name] = stripe_customer.name if stripe_customer.respond_to?(:name) && !stripe_customer.name.blank?
+        end
+        properties_json
+      rescue => e
+        Sentry.capture_exception(e)
+        properties_json
       end
 
     end
