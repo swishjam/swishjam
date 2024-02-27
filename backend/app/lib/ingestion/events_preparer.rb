6dd8fe5f49054f516139fcabb813249e724233a7
Collin Schneider <collin.thomas.schneider@gmail.com>
2024-02-27 08:19:17 -0800

    capture explicit exception during event preparation failures

--- backend/app/lib/ingestion/events_preparer.rb
+++ backend/app/lib/ingestion/events_preparer.rb
@@ -59,7 +59,8 @@ module Ingestion
       ingestion_batch.num_successful_records += prepared_events.count
       ingestion_batch.save! if (@update_ingestion_batch_every_n_iterations || 0) > 0 && (ingestion_batch.num_successful_records + ingestion_batch.num_failed_records) % @update_ingestion_batch_every_n_iterations == 0
     rescue => e
-      Sentry.capture_message("Error preparing event into ingestion format during events ingestion, continuing with the rest of the events in the queue and pushing this one to the DLQ.\nerror: #{e.message}", level: 'error', extra: { event_json: event_json })
+      Sentry.capture_exception(e)
+      Sentry.capture_message("Error preparing event into ingestion format during events ingestion, continuing with the rest of the events in the queue and pushing this one to the DLQ.\nerror: #{e.message}", extra: { event_json: event_json })
       event_json['dlq_data'] = { error_message: e.message, errored_at: Time.current }
       @failed_events << event_json
       ingestion_batch.num_failed_records += 1
